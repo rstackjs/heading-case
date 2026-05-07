c9475232a53c84a144eefb6e4d3478b945d6edee
neverland <chenjiahan.jait@bytedance.com>
2026-05-07 19:02:58 +0800

    chore: migrate linting to Rslint and Prettier

--- src/dict.ts
+++ src/dict.ts
@@ -1,6 +1,4 @@
-import abbreviates from 'case-police/dict/abbreviates.json' with {
-  type: 'json',
-};
+import abbreviates from 'case-police/dict/abbreviates.json' with { type: 'json' };
 import brands from 'case-police/dict/brands.json' with { type: 'json' };
 import general from 'case-police/dict/general.json' with { type: 'json' };
 import products from 'case-police/dict/products.json' with { type: 'json' };
